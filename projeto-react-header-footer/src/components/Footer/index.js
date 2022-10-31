3fffab91109bb3a5bb00d13bcd0d813a7d0777d3
Flora Fernandes Martins <83349315+florafernandes@users.noreply.github.com>
2022-10-31 16:57:13 -0300

    Projeto-react-header-footer
    Criação de componentes React

---
+++ projeto-react-header-footer/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer(){
+
+    return(
+        <>
+            Todos os direitos reservados
+        </>
+    );
+}
+
+export default Footer;
---
+++ projeto-react-header-footer/src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <>
+            <div>
+                Topo do Site
+            </div>
+        </>
+    );
+}
+
+export default Header;
