ad622049af95f9e95ef71c85854687b6a99b9938
Flora Fernandes Martins <83349315+florafernandes@users.noreply.github.com>
2022-11-07 17:29:32 -0300

    Add files via upload

---
+++ Teste Pagina React/src/componentes/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from './imgs/reacticon.png'
+
+export default function App(){
+    return(
+        <header>
+            <img src={Logo}/>
+            <h1>Pagina Teste</h1>
+
+        </header>
+    )
+}
