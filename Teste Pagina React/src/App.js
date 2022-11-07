daac3fe68c90e30a4e78a1fcce9b0614cde2f832
Flora Fernandes Martins <83349315+florafernandes@users.noreply.github.com>
2022-11-07 17:09:42 -0300

    Add files via upload

--- Teste Pagina React/src/App.js
+++ Teste Pagina React/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './componentes/Header'
 import Corpo from './componentes/Corpo'
 
-
+// Alteracao teste
 export default function App(){
   return(
     <>
