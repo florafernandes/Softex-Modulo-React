5dc065d34aa9bf83e1877a175ae625fb39f2e7a8
Flora Fernandes Martins <83349315+florafernandes@users.noreply.github.com>
2022-12-12 17:40:43 -0300

    Update App.js

--- hook usestate/src/App.js
+++ hook usestate/src/App.js
@@ -4,22 +4,13 @@ function App() {
 
   const [ valor, setValor ] = useState(0);
 
-  function funcAumentar(){
-    setValor(valor + 1);
-  }
-
-  function funcDiminuir(){
-    setValor(valor - 1);
-  }
-
   return (
-
-    <>
-      <div>Quantidade de produtos no carrinho: <b>{ valor }</b></div>
-      <button onCLick={funcAumentar}>Adicionar + 1 no carrinho</button>
-      <button onCLick={funcDiminuir}>Remover 1 no carrinho</button>
-    </>
-
+    <div>
+      <p>Você clicou {valor} vezes.</p>
+      <button onClick={() => setValor(valor + 1)}>
+        Clique
+      </button>
+    </div>
   );
 }
 
